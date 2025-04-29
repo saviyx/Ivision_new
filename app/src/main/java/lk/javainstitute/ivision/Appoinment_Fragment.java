31e15600188ca2ac3e29f7ed3b1ce69a68b5b9b2
Tharusha12-se <146365731+Tharusha12-se@users.noreply.github.com>
2025-04-29 19:05:40 +0530

    2

--- app/src/main/java/lk/javainstitute/ivision/Appoinment_Fragment.java
+++ app/src/main/java/lk/javainstitute/ivision/Appoinment_Fragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Appoinment_Fragment extends Fragment {
+public class Appoinment_Fragment extends Fragment  {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
