4af24b35c330a8b520ae3667e2911fdf5c214274
Tharusha12-se <146365731+Tharusha12-se@users.noreply.github.com>
2025-05-02 02:08:50 +0530

    10

--- app/src/main/java/lk/javainstitute/ivision/Admin/Add_Admin.java
+++ app/src/main/java/lk/javainstitute/ivision/Admin/Add_Admin.java
@@ -3,64 +3,40 @@ package lk.javainstitute.ivision.Admin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import lk.javainstitute.ivision.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Add_Admin#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Add_Admin extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Add_Admin() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Add_Admin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Add_Admin newInstance(String param1, String param2) {
-        Add_Admin fragment = new Add_Admin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add__admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_add__admin, container, false);
+
+        Button addAdminButton = view.findViewById(R.id.add_admin);
+        addAdminButton.setOnClickListener(v -> {
+
+            Fragment addAdminFragment = new New_Admin_Save();
+
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+
+            transaction.hide(Add_Admin.this);
+
+            transaction.add(R.id.main_scrollview1,addAdminFragment);
+
+            transaction.addToBackStack(null);
+            transaction.commit();
+
+        });
+
+        return view;
     }
 }
