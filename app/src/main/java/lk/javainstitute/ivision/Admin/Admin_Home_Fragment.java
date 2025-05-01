808518d9be32f23f1ed33cba2405b97b2f09c669
saviyx <146174880+saviyx@users.noreply.github.com>
2025-05-02 02:01:40 +0530

    change 26

--- app/src/main/java/lk/javainstitute/ivision/Admin/Admin_Home_Fragment.java
+++ app/src/main/java/lk/javainstitute/ivision/Admin/Admin_Home_Fragment.java
@@ -11,9 +11,11 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import lk.javainstitute.ivision.Appoinment_Fragment;
+import lk.javainstitute.ivision.Eye_Test_Fragment;
 import lk.javainstitute.ivision.Glass_Fragment;
 import lk.javainstitute.ivision.Home_Fragment;
 import lk.javainstitute.ivision.R;
+import lk.javainstitute.ivision.Review_Fragment;
 
 
 public class Admin_Home_Fragment extends Fragment {
@@ -75,6 +77,103 @@ public class Admin_Home_Fragment extends Fragment {
             transaction.commit();
 
         });
+
+        LinearLayout reportsCard = view.findViewById(R.id.reports);
+
+        reportsCard.setOnClickListener(v -> {
+            Fragment reportFragment = new Admin_Report();
+
+
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+
+            // Hide this fragment
+            transaction.hide(Admin_Home_Fragment.this);
+
+            // Add the detail fragment to your activity container (NOT the linear layout)
+            // Make sure R.id.mainFragmentContainer is in your activity layout
+            transaction.add(R.id.main_scrollview1, reportFragment);
+
+            // Add to back stack so pressing BACK will remove detail and unhide this
+            transaction.addToBackStack(null);
+
+            // Commit
+            transaction.commit();
+
+
+        });
+
+        LinearLayout paymentsCard = view.findViewById(R.id.payment);
+        paymentsCard.setOnClickListener(v ->{
+            Fragment paymentFragment = new Payment_History_Fragment();
+
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+
+            transaction.hide(Admin_Home_Fragment.this);
+
+            transaction.add(R.id.main_scrollview1,paymentFragment);
+
+            transaction.addToBackStack(null);
+            transaction.commit();
+
+        });
+
+        LinearLayout addAdminCard = view.findViewById(R.id.add_admin);
+
+        addAdminCard.setOnClickListener(v -> {
+            Fragment addAdminFragment = new Add_Admin();
+
+
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+
+            // Hide this fragment
+            transaction.hide(Admin_Home_Fragment.this);
+
+            // Add the detail fragment to your activity container (NOT the linear layout)
+            // Make sure R.id.mainFragmentContainer is in your activity layout
+            transaction.add(R.id.main_scrollview1, addAdminFragment);
+
+            // Add to back stack so pressing BACK will remove detail and unhide this
+            transaction.addToBackStack(null);
+
+            // Commit
+            transaction.commit();
+
+
+        });
+
+        LinearLayout reviewCard = view.findViewById(R.id.review);
+
+        reviewCard.setOnClickListener(v -> {
+            Fragment reviewFragment = new Review_Fragment();
+
+
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+
+            // Hide this fragment
+            transaction.hide(Admin_Home_Fragment.this);
+
+            // Add the detail fragment to your activity container (NOT the linear layout)
+            // Make sure R.id.mainFragmentContainer is in your activity layout
+            transaction.add(R.id.main_scrollview1, reviewFragment);
+
+            // Add to back stack so pressing BACK will remove detail and unhide this
+            transaction.addToBackStack(null);
+
+            // Commit
+            transaction.commit();
+
+
+        });
+
+
 
         // Inflate the layout for this fragment
         return view;
