ed2a287f5090959db4e795bbd2abd09f958300bf
Sazul19 <ss.senarathne0919@gmail.com>
2025-05-02 10:39:51 +0530

    review ui+model cm

--- app/src/main/java/lk/javainstitute/ivision/ReviewDialogueBox.java
+++ app/src/main/java/lk/javainstitute/ivision/ReviewDialogueBox.java
@@ -1,34 +1,65 @@
-package lk.javainstitute.ivision;
-
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.LayerDrawable;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.RatingBar;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ReviewDialogueBox extends AppCompatActivity {
-    private RatingBar ratingBar;
-    private Button button;
-    private TextView editTextReview;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.review_dialogue_box);
-
-        ratingBar = findViewById(R.id.ratingBar);
-        button = findViewById(R.id.button);
-        editTextReview = findViewById(R.id.editTextReview);
-
-        button.setOnClickListener(view -> {
-
-            editTextReview.setText("You Rated : " + ratingBar.getRating());
-            LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
-            stars.getDrawable(2).setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
-        });
-    }
-}
+//package lk.javainstitute.ivision;
+//
+//import android.graphics.Color;
+//import android.graphics.PorterDuff;
+//import android.graphics.drawable.LayerDrawable;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.RatingBar;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.google.firebase.Firebase;
+//import com.google.firebase.firestore.CollectionReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//
+//import lk.javainstitute.ivision.model.Review;
+//
+//public class ReviewDialogueBox extends AppCompatActivity {
+//    private RatingBar ratingBar;
+//    private Button button;
+//    private TextView editTextReview;
+//    private FirebaseFirestore db;
+//
+//    private CollectionReference reviCollection;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.review_dialogue_box);
+//
+//        ratingBar = findViewById(R.id.ratingBar);
+//        button = findViewById(R.id.button);
+//        editTextReview = findViewById(R.id.editTextReview);
+//
+//        db=FirebaseFirestore.getInstance();
+//        reviCollection=db.collection("Review");
+//
+//
+//        button.setOnClickListener(view -> {
+//
+//            editTextReview.setText("You Rated : " + ratingBar.getRating());
+//            LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+//            stars.getDrawable(2).setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
+//        });
+//    }
+//
+//    private void submitReview(EditText editTextReview, RatingBar ratingBar){
+//        String reviewText=editTextReview.getText().toString();
+//        float rating=ratingBar.getRating();
+//
+//        Review review=new Review(reviewText,rating);
+//        reviCollection.add(review).addOnSuccessListener(documentReference -> {
+//            Toast.makeText(this, "Review submitted successfully", Toast.LENGTH_SHORT).show();
+//            ratingBar.setRating(0);
+//            reviewText.setText("");
+//        }).addOnFailureListener(e -> {
+//            Toast.makeText(this, "Failed to submit review", Toast.LENGTH_SHORT).show();
+//        });
+//
+//        }
+//    }
+//}
--- app/src/main/java/lk/javainstitute/ivision/model/Review.java
+++ app/src/main/java/lk/javainstitute/ivision/model/Review.java
@@ -1,6 +1,8 @@
 package lk.javainstitute.ivision.model;
 
 
+import com.google.firebase.Timestamp;
+
 public class Review {
     private String id;
     private String userName;
@@ -10,7 +12,7 @@ public class Review {
 
     public Review() {}
 
-    public Review(String userName, float rating, String reviewText) {
+    public Review(String reviewText, float rating) {
         this.userName=userName;
         this.rating=rating;
         this.reviewText=reviewText;
@@ -19,4 +21,43 @@ public class Review {
     }
 
 
+    //gettes and setters
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName){
+        this.userName=userName;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+    public String getReviewText(){
+        return reviewText;
+    }
+    public void setReviewText(String reviewText){
+        this.reviewText=reviewText;
+
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Timestamp timestamp){
+        this.timestamp=timestamp;
+    }
 }
